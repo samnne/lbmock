96478be3596c2685b309db4eebb17bee325fdebf
Samuel Echefu <samuelnnechefu@gmail.com>
2025-11-18 17:34:42 -0800

    added load balancers and weighted distrubution and auto balance with new server creation

--- index.js
+++ index.js
@@ -13,17 +13,34 @@ const servers = [
   new Server("D"),
 ];
 
+const makeNewServer = (newID) => {
+  servers.push(new Server(newID));
+};
+
+const removeServer = (ID) => {
+  if (servers.length <= 2) {
+    return;
+  }
+
+  servers.pop();
+};
+
 const rr = new LoadBalancer(servers, roundRobin);
 const lc = new LoadBalancer(servers, leastConnections);
 const wt = new LoadBalancer(servers, weighted);
 
 const runApp = async (loadB) => {
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 500; i++) {
     loadB.handleRequest(i).then((res) => {
       console.log(
         `${loadB.name} Request ID ${res.requestId} ${res.server} [${res.time}ms] `
       );
     });
+    if (i / servers.length > servers.length * 2) {
+      makeNewServer(servers.length + 1);
+    } else {
+      removeServer();
+    }
   }
 };
 // Round Robin
